b7f48e2b4d1c45f40bb2a007f8c030e60ec267b0
ThomasGrc <thomas.garcia@epitech.eu>
2026-01-25 20:58:51 +0100

    navbar on reco page

--- src/app/components/recommandations-page/recommandations-page.ts
+++ src/app/components/recommandations-page/recommandations-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Contact } from "../contact/contact";
+import { Navbar } from "../navbar/navbar";
 
 @Component({
   selector: 'app-recommandations-page',
-  imports: [Contact],
+  imports: [Contact, Navbar],
   templateUrl: './recommandations-page.html',
   styleUrl: './recommandations-page.scss',
 })
